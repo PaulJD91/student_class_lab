f31252097ff2baf48ac7128f3d5c8595d391e9e7
Paul <paul.dennehy@hotmail.com>
2023-07-10 13:42:31 +0100

    First commit after starting

---
+++ runner.py
@@ -0,0 +1,4 @@
+from students import CodeClanStudent
+
+student = CodeClanStudent("Paul", "E65")
+print(student.student_name)
---
+++ students.py
@@ -0,0 +1,7 @@
+class CodeClanStudent:
+
+    def __init__(self, student_name, cohort):
+        self.student_name = student_name
+        self.cohort = cohort
+
+    
