140d5352e10c204dbf35ea4ab1d8da508ee27020
Paul <paul.dennehy@hotmail.com>
2023-07-10 14:23:02 +0100

    started task b

--- runner.py
+++ runner.py
@@ -1,7 +1,17 @@
-from students import Student
+# from students import Student
 
-student = Student("Paul", "E65")
-print(student.student_name)
+# student = Student("Paul", "E65")
+# print(student.student_name)
 
-student.talk()
-student.fav_language("python")
+# student.talk()
+# student.fav_language("python")
+
+from team import Team
+
+team = {
+    "name": "Liverpool"
+    "players": [ "Gerrard",
+                "Carragher",
+                "Alonso"]
+    "coach": "Benitez"
+}
